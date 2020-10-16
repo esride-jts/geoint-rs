47f8e160e8ad1e83c91d828d971575eb4c498301
Jan Tschada <jts@esri-de.com>
2020-10-16 13:53:39 +0200

    Using concave and is convex

--- geoint-rs/src/lib.rs
+++ geoint-rs/src/lib.rs
@@ -1,8 +1,9 @@
 mod traits;
 
-use geo::{Coordinate, Point, Polygon, GeometryCollection};
+use geo::{Coordinate, MultiPoint, Point, Polygon, GeometryCollection};
 use geo::concave_hull::ConcaveHull;
 use geo::convex_hull::ConvexHull;
+use geo::algorithm::is_convex::IsConvex;
 
 pub struct GeointTool {
 
@@ -25,6 +26,15 @@ impl traits::GpTool<bool> for Polygon<f64> {
 }
 
 
+
+/// Concave hull implementation
+fn concave_hull_points(points: Vec<Point<f64>>) -> Polygon<f64> {
+    let multi_point = MultiPoint(points);
+    let concavity = 2.0;
+    multi_point.concave_hull(concavity)
+}
+
+
 #[cfg(test)]
 mod tests {
 
@@ -36,5 +46,19 @@ mod tests {
         let tool = GeointTool {};
         let executed = tool.execute();
         assert_eq!(true, executed);
+    }
+
+    #[test]
+    fn multipoint_test() {
+        let points = vec![
+            Point::new(-102.130629, 48.251542),
+            Point::new(-102.07351, 48.3250820000001),
+            Point::new(-101.20723, 46.253469),
+            Point::new(-103.328183, 46.243274),
+            Point::new(-101.20723, 46.253469)
+        ];
+
+        let hull = concave_hull_points(points);
+        assert_eq!(true, hull.exterior().is_convex());
     }
 }
