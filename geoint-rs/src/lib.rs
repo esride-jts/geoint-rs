701c3f076e477a0af0e47f1d7548f987f3b09db5
Jan Tschada <jts@esri-de.com>
2020-10-16 11:48:28 +0200

    Concave hull tryout

--- geoint-rs/src/lib.rs
+++ geoint-rs/src/lib.rs
@@ -1,5 +1,9 @@
 mod traits;
 
+use geo::{Coordinate, Point, Polygon, GeometryCollection};
+use geo::concave_hull::ConcaveHull;
+use geo::convex_hull::ConvexHull;
+
 pub struct GeointTool {
 
 }
@@ -7,6 +11,15 @@ pub struct GeointTool {
 impl traits::GpTool<bool> for GeointTool {
 
     fn execute(&self) -> bool {
+        true
+    }
+}
+
+impl traits::GpTool<bool> for Polygon<f64> {
+
+    fn execute(&self) -> bool {
+        let concavity = 2.0;
+        self.concave_hull(concavity);
         true
     }
 }
